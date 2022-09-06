c72619c7658ed51c8bacf9bd57667484c2370fd3
adrianomikio <adrianouge@gmail.com>
2022-09-06 14:36:46 -0300

    Exercicio 3 feito.

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,11 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Qual a sua nacionalidade? Primeira letra maiúscula, por favor.")
+
+if(nacionalidade === "Brasileira" || "Argentina" || "Uruguaia" || "Colombiana" || "Chilena")
+
+{
+    console.log(`Sua nacionalidade é: ${nacionalidade}.`)
+}
+
+else {
+    console.log("Nacionalidade não encontrada.")
+}
