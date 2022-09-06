73be2149ef704d9a3893f5b08b1fd2d83ff5e200
adrianomikio <adrianouge@gmail.com>
2022-09-06 14:33:56 -0300

    Exercicio 2 feito.

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,27 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = 18
+let TerminouEnsinoMedio = false
+let isCursandoOutraFaculdade = false
 
-if(){
+if(idade >= 18) {
+                console.log("É maior de idade.")
+}
 
-}else{
-	
+else {
+    console.log("Não é maior de idade.")
+}
+
+if(TerminouEnsinoMedio === true){
+                     console.log("Terminou ensino médio.")
+}
+
+else {
+    console.log("Não terminou ensino médio.")
+}
+
+if(isCursandoOutraFaculdade === true) {
+                                   console.log("Está cursando faculdade.")
+}
+
+else {
+    console.log("Não está cursando faculdade.")
 }
